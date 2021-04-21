64aa87835c441fb89fcf12aaa3ac5d854c2b4b2e
unknown <Richard Stg>
2021-04-21 18:19:26 +0100

    Created views for dice game

--- resources/views/dicegameinit.blade.php
+++ resources/views/dicegameinit.blade.php
@@ -4,6 +4,16 @@
 
 @section('content')
 
-    <h1>Tärningsspelet</h1>
+    <h1>Starta spelet</h1>
+    <div>
+        <form action="" method="post">
+            <label for="numberDices">Antal tärningar:</label>
+            <select name="numberDices" id="numberDices">
+                <option value="1" selected="selected">1</option>
+                <option value="2">2</option>
+            </select>
+            <input type="submit" name="startGame" value="Starta spelet">
+        </form>
+    </div>
 
 @endsection
---
+++ resources/views/dicegameplay.blade.php
@@ -0,0 +1,34 @@
+@extends('master')
+
+@section('title', 'Tärningsspelet')
+
+@section('content')
+
+    <h1>Spela</h1>
+    <div class="total-points">
+        <h3 style="border-bottom: none">Dina poäng för rundan: {{ $totalPointsPlayer }}</h3>
+        @if($playerLastHandSum)
+            <h3 style="border-bottom: none">Poäng för senaste kast: {{ $playerLastHandSum }}</h3>
+        @endif
+    </div>
+    <form action="play" method="post">
+        <input type="submit" name="rollDices" value="Kasta">
+    </form>
+    @if($totalPointsPlayer > 0)
+        <form action="computerplay" method="post">
+            <input type="submit" name="stopRound" value="Stanna">
+        </form>
+    @endif
+
+    <div class="game-field">
+    @if($playerLastHandSum)
+        <p class="dice-utf8">
+            @foreach($playerLastHandGraphical as $value)
+                <i style="font-style: normal" class="{{ $value }}"></i>
+            @endforeach
+        </p>
+        <p>Du kastade: {{ implode(', ', $playerLastHand) }}</p>
+    @endif
+    </div>
+
+@endsection
